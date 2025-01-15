939428cc5ce3ca41d7205f33a010897caa345358
Sayfudin90 <Seifdin62@gmail.com>
2025-01-15 08:54:02 +0300

    submitting-assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React from 'react';
+import Navbar from './Navbar'; 
+import Home from './Home';
+import About from './About';
 
 function NavBar() {
   return (
@@ -8,13 +11,6 @@ function NavBar() {
   );
 }
 
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
 
 {/* write an <About> component here */}
 
@@ -23,8 +19,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div id="home">
+      <h2>Home</h2>
+      <p>Welcome to the homepage!</p>
+    </div>
+  );
+}
+
+export default Home;
